58b5d4c6b6918a2778d19d731429cd6db8da6060
edoatley <edoatley@gmail.com>
2017-03-18 10:59:15 +0000

    Initial commit

---
+++ src/main/java/edoatley/example/MessagingAndRestApplication.java
@@ -0,0 +1,12 @@
+package edoatley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAndRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingAndRestApplication.class, args);
+	}
+}
