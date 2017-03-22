8f61e68e7917d026003099051594a178afee95b9
Edd <edoatley@gmail.com>
2017-03-22 19:42:30 +0000

    Add CommandLineRunner to be able to load up in memory database with initial values

--- src/main/java/edoatley/example/MessagingAndRestApplication.java
+++ src/main/java/edoatley/example/MessagingAndRestApplication.java
@@ -1,14 +1,42 @@
 package edoatley.example;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import edoatley.example.payment.Payment;
+import edoatley.example.persist.PaymentRepository;
 
 @SpringBootApplication
 public class MessagingAndRestApplication {
 
 	//private static final Logger log = LoggerFactory.getLogger(MessagingAndRestApplication.class);
 	
+	@Autowired
+	PaymentRepository paymentRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingAndRestApplication.class, args);
+	}
+	
+	public CommandLineRunner init(String... args) {
+		return  (evt) -> {
+			// need to add code to convert the list into neat Payments to bung into save
+			Random rand = new Random();
+			List<String> listOfNamesToGeneratePaymentsFor = Arrays.asList("name1", "name2", "name3");
+			paymentRepository.save(listOfNamesToGeneratePaymentsFor
+										.stream()
+				    					.map(s -> new Payment(LocalDate.now(), s, BigDecimal.valueOf(rand.nextInt(5000000), 2)))
+				    					.collect(Collectors.toList()));
+			
+		};
 	}
 }
