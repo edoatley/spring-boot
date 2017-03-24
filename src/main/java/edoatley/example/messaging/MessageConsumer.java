ac7effa3b8c92671e636020ca675cf6f4a9b90ce
Edd <edoatley@gmail.com>
2017-03-24 14:43:47 +0000

    trying to receive arraylist

--- src/main/java/edoatley/example/messaging/MessageConsumer.java
+++ src/main/java/edoatley/example/messaging/MessageConsumer.java
@@ -1,11 +1,15 @@
 package edoatley.example.messaging;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import edoatley.example.payment.Payment;
 
 @Component
 public class MessageConsumer {
 
-	public void receiveMessage(String message) {
+	public void receiveMessage(List<Payment> message) {
 		System.out.println("MessageConsumer.receiveMessage --> " + message);
 	}
 } 
