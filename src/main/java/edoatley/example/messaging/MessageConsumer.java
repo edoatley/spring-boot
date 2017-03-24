3378517fe5042c5e306e4b109f8c606f5aad3b05
Edd <edoatley@gmail.com>
2017-03-24 14:59:34 +0000

    correct sysout to logged message and the generics

--- src/main/java/edoatley/example/messaging/MessageConsumer.java
+++ src/main/java/edoatley/example/messaging/MessageConsumer.java
@@ -2,14 +2,17 @@ package edoatley.example.messaging;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import edoatley.example.payment.Payment;
 
 @Component
 public class MessageConsumer {
-
-	public void receiveMessage(List<Payment> message) {
-		System.out.println("MessageConsumer.receiveMessage --> " + message);
+	private static final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
+	
+	public void receiveMessage(List<String> messages) {
+		for (String message : messages) {			
+			log.error("MessageConsumer.receiveMessage --> " + message);
+		}
 	}
 } 
