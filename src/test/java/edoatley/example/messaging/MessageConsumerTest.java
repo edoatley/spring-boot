5ddbcbc8b3e67e37085fe9904b7798b7be390281
Edd <edoatley@gmail.com>
2017-03-24 17:48:33 +0000

    reproduced actual error seen with curl command

--- src/test/java/edoatley/example/messaging/MessageConsumerTest.java
+++ src/test/java/edoatley/example/messaging/MessageConsumerTest.java
@@ -6,13 +6,18 @@ import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edoatley.example.payment.Payment;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MessageConsumerTest {
 
 	// class under test
